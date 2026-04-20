433604861c9746607df7607a57fa50b52c84b3da
Nikolai T. Jensen <nitje@itm8.com>
2026-04-20 15:21:17 +0200

    Fix granian

--- psst_secret/wsgi.py
+++ psst_secret/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from granian.utils.proxies import wrap_wsgi_with_proxy_headers
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "psst_secret.settings")
 
 application = get_wsgi_application()
+# We assume that people are using sane reverse proxies that set the standard headers, so we trust all hosts here.
+application = wrap_wsgi_with_proxy_headers(
+    application, trusted_hosts=os.environ.get("GRANIAN_TRUSTED_HOSTS", "*").split(",")
+)
